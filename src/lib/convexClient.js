59955af182673ba1224bb12d649c32a6eb989b7f
Anuj Kumar <kumaranujranchi@gmail.com>
2026-05-23 00:55:00 +0530

    Refactor: complete migration to Pure Convex and removal of local SQLite implementation

--- src/lib/convexClient.js
+++
@@ -1,8 +0,0 @@
-import { ConvexHttpClient } from "convex/browser";
-import { api } from "../../convex/_generated/api";
-
-const url = process.env.NEXT_PUBLIC_CONVEX_URL;
-export const isConvexEnabled = !!url;
-
-export const convex = isConvexEnabled ? new ConvexHttpClient(url) : null;
-export { api };
